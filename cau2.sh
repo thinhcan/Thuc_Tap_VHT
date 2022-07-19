af8093557ce88c9bbdb5ab6127efde41f689536c
thinhcan <canquangthinh@gmail.com>
2022-07-19 14:21:32 +0700

    bash script

--- cau2.sh
+++ cau2.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/bash
 
 i=0
-val=1000000
+val=100
 # while [ $i -lt 5 ]
 # do
     > time_and_interval.txt
     echo  "$val" > "freq.txt"
-    timeout 5s ./code_test
+    timeout 5s ./last_code
 #     ((val/=10))
 #     ((i++))
     
