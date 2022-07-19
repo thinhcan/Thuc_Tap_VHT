675e2d702b0ff5edae2af5bd7f6e15439af6a757
thinhcan <canquangthinh@gmail.com>
2022-07-19 14:26:00 +0700

    another

--- cau2.sh
+++ cau2.sh
@@ -2,17 +2,13 @@
 
 i=0
 val=100
-# while [ $i -lt 5 ]
-# do
+ while [ $i -lt 5 ]
+ do
     > time_and_interval.txt
     echo  "$val" > "freq.txt"
-<<<<<<< HEAD
-    timeout 5s ./last_code
-=======
-    timeout 60s ./code_test
->>>>>>> 448e14093feeb964c014d366d6f82d963b4f6fc2
-#     ((val/=10))
-#     ((i++))
+    timeout 60s ./last_code
+     ((val/=10))
+     ((i++))
     
-# done
+ done
 
