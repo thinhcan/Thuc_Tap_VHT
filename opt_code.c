65f6cd27fba0df66d23856bbdb2220f35b43da95
thinhcan <canquangthinh@gmail.com>
2022-08-28 14:41:50 +0700

    them code toi uu

--- opt_code.c
+++ opt_code.c
@@ -13,7 +13,6 @@ static struct timespec ot;
  FILE *file;
  FILE *fp;
 
-//pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
 
 long T =100000;
 
@@ -23,21 +22,10 @@ void * sam_func(void *arg) {
         sp.sched_priority = 70;
         pthread_setschedparam(pthread_self(), SCHED_FIFO, &sp);
 
-       clock_gettime(CLOCK_MONOTONIC, &request);
-
+        clock_gettime(CLOCK_MONOTONIC, &request);
+        
         while(1) {
       
-           // printf("sam\n");
-     //  printf("sam count %d\n",count_sam);
-
-        // pthread_mutex_lock(&mtx);
-        
-        // while(input_flag == 0) {
-        //         pthread_cond_wait(&condition_input,&mtx);
-        // }
-        
-        //pthread_mutex_unlock(&mtx);
-
         request.tv_nsec += T;
        
         if(request.tv_nsec > 1000*1000*1000) {
@@ -45,30 +33,9 @@ void * sam_func(void *arg) {
                 request.tv_sec++;
         }
 
- 
-
         clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &request, NULL);
         
-        
-     //   pthread_mutex_lock(&mutx);
         clock_gettime(CLOCK_MONOTONIC, &rtc);
-        //flag = 1;
-    //    pthread_mutex_unlock(&mutx);
-       //pthread_cond_signal(&cv);
-        
-        
-//         pthread_mutex_lock(&mtx);
-        
-//         input_flag = 0;
-//         sample_flag = 1;
-//        // count_sam ++;
-
-//         pthread_cond_signal(&condition_sample);
-
-//         pthread_mutex_unlock(&mtx);
-
-
-
         }
         
    return NULL;
@@ -81,8 +48,7 @@ void * in_func(void *arg) {
 
         fp = fopen("freq.txt","r");
        while(1) {
-      //  printf("in\n");
-        
+     
         char buff[100];
         fgets(buff,sizeof(buff),fp);
         char *eptr;
@@ -102,37 +68,33 @@ void    *log_func(void* arg) {
          file = fopen("time_and_interval.txt","a+");
         while(1) {
 
-       // printf("log\n");
-     //   pthread_mutex_lock(&mutx);
-     //   while (flag == 0)s
-       //          pthread_cond_wait(&cv, &mutx);
+                if(rtc.tv_nsec != ot.tv_nsec ||rtc.tv_sec != ot.tv_sec ) {
+        
+                        if(file == NULL) {
+                                printf("ko mo dc file\n");
+                                return NULL;
+                        }
+
+                        long diff_sec = (long)rtc.tv_sec - (long)ot.tv_sec ;
+                        long diff_nsec;
+                        
+                        if(rtc.tv_nsec > ot.tv_nsec) {
+                                diff_nsec = rtc.tv_nsec - ot.tv_nsec;
+                        }
+                        
+                        else {
+                                diff_nsec = 1000000000 - ot.tv_nsec + rtc.tv_nsec;
+                                diff_sec = diff_sec - 1;
+                        }        
+                        
+                        if(ot.tv_nsec != 0) {
+
+                                fprintf(file, "%ld\n",diff_nsec);
+                        }
                 
-        if(rtc.tv_nsec != ot.tv_nsec ||rtc.tv_sec != ot.tv_sec ) {
-       
-        if(file == NULL) {
-                printf("ko mo dc file\n");
-                return NULL;
-        }
-        long diff_sec = (long)rtc.tv_sec - (long)ot.tv_sec ;
-        long diff_nsec;
-        if(rtc.tv_nsec > ot.tv_nsec) {
-                diff_nsec = rtc.tv_nsec - ot.tv_nsec;
-        }
-        else {
-                diff_nsec = 1000000000 - ot.tv_nsec + rtc.tv_nsec;
-                diff_sec = diff_sec - 1;
-        }        
-        if(ot.tv_nsec != 0) {
-
-                fprintf(file, "%ld\n",diff_nsec);
-        }
-        
-        ot.tv_nsec = rtc.tv_nsec;
-        ot.tv_sec = rtc.tv_sec;
-        }
-
-        
-        //pthread_mutex_unlock(&mutx);
+                        ot.tv_nsec = rtc.tv_nsec;
+                        ot.tv_sec = rtc.tv_sec;
+                }
         }
         fclose(file);
         return NULL;
@@ -148,7 +110,6 @@ request.tv_nsec = 0;
 
 pthread_create(&INPUT, NULL, &in_func, NULL); //Tạo luồng input
 pthread_create(&SAMPLE,  NULL,&sam_func, NULL); //Tạo luồng sample
-//pthread_create(&SAMPLE, NULL, &sam_func, NULL); //Tạo luồng sample
 pthread_create(&LOGIN, NULL, &log_func, NULL); //Tạo luồng login
 
 pthread_join(INPUT,NULL);
